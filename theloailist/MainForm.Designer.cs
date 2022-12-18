859882e0289234c6acb9b77f5ef5aa7cd76c2f1b
Tom Gregor <tgregor@ivsoftware.com>
2022-12-18 12:00:26 -0700

    Add project files.

---
+++ theloailist/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace theloailist
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TheLoaiGrid = new System.Windows.Forms.DataGridView();
+            this.buttonChangeValueTest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TheLoaiGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TheLoaiGrid
+            // 
+            this.TheLoaiGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TheLoaiGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TheLoaiGrid.Location = new System.Drawing.Point(0, 0);
+            this.TheLoaiGrid.Name = "TheLoaiGrid";
+            this.TheLoaiGrid.RowHeadersWidth = 62;
+            this.TheLoaiGrid.RowTemplate.Height = 33;
+            this.TheLoaiGrid.Size = new System.Drawing.Size(478, 199);
+            this.TheLoaiGrid.TabIndex = 0;
+            // 
+            // buttonChangeValueTest
+            // 
+            this.buttonChangeValueTest.Location = new System.Drawing.Point(0, 205);
+            this.buttonChangeValueTest.Name = "buttonChangeValueTest";
+            this.buttonChangeValueTest.Size = new System.Drawing.Size(177, 34);
+            this.buttonChangeValueTest.TabIndex = 1;
+            this.buttonChangeValueTest.Text = "ChangeValueTest";
+            this.buttonChangeValueTest.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.buttonChangeValueTest);
+            this.Controls.Add(this.TheLoaiGrid);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.TheLoaiGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TheLoaiGrid;
+        private System.Windows.Forms.Button buttonChangeValueTest;
+    }
+}
+
---
+++ theloailist/MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace theloailist
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()=> InitializeComponent();
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            TheLoaiGrid.AllowUserToAddRows = false;
+            TheLoaiGrid.DataSource = TheLoaiList;
+
+            // Add a few test items.
+            TheLoaiList.Add(new TheLoai { SoTuaSach = "1", Column2 = "A", });
+            TheLoaiList.Add(new TheLoai { SoTuaSach = "1", Column2 = "B", });
+            TheLoaiList.Add(new TheLoai { SoTuaSach = "1", Column2 = "C", });
+
+            #region F O R M A T    C O L U M N S
+            foreach (DataGridViewColumn column in TheLoaiGrid.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            #endregion F O R M A T    C O L U M N 
+
+            buttonChangeValueTest.Click += onChangeValueTestClick;
+        }
+
+        int _testCount = 10;
+        private void onChangeValueTestClick(object sender, EventArgs e)
+        {
+            // Now perform change the value on the Data not the DataGridView
+            var testValue = _testCount++.ToString();
+            foreach (TheLoai theLoai in TheLoaiList)
+            {
+                theLoai.SoTuaSach = testValue; 
+            }
+        }
+
+        BindingList<TheLoai> TheLoaiList { get; } = new BindingList<TheLoai>();
+    }
+
+    // using System.Runtime.CompilerServices;
+    class TheLoai : INotifyPropertyChanged
+    {
+        string _soTuaSach = string.Empty;
+        public string SoTuaSach
+        {
+            get => _soTuaSach;
+            set
+            {
+                if (!Equals(_soTuaSach, value))
+                {
+                    _soTuaSach = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        string _column2 = string.Empty;
+        public string Column2
+        {
+            get => _column2;
+            set
+            {
+                if (!Equals(_column2, value))
+                {
+                    _column2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
